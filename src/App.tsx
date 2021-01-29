682b2dc0b9da1e1245270855a556416f8d090c31
Alexey Sachko <asachko@gmini.tech>
2021-01-29 14:41:48 +0500

    fixed babel, added dependnecy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { ServiceProvider } from "./market-depth/services/ctx";
+import { DepthService } from "./market-depth/services/depth.service";
 import { MarketDepth } from "./market-depth/view";
+
+const depthService = new DepthService();
 
 export const App = React.memo(() => {
   return (
     <div className="root-container">
-      <MarketDepth />
+      <ServiceProvider value={depthService}>
+        <MarketDepth />
+      </ServiceProvider>
     </div>
   );
 });
--- src/market-depth/services/ctx.ts
+++ src/market-depth/services/ctx.ts
@@ -3,6 +3,8 @@ import { DepthService } from "./depth.service";
 
 const ServiceCTX = React.createContext<DepthService>(undefined as any);
 
+export const ServiceProvider = ServiceCTX.Provider;
+
 export const useDepthService = (): DepthService => {
   return React.useContext(ServiceCTX);
 };
