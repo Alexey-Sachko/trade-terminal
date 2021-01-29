ccfb7540f00f3d437fa781684a64c3b0445019fd
Alexey Sachko <asachko@gmini.tech>
2021-01-29 14:47:18 +0500

    move app

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { ServiceProvider } from "./market-depth/services/ctx";
-import { DepthService } from "./market-depth/services/depth.service";
-import { MarketDepth } from "./market-depth/view";
+import { ServiceProvider } from "../market-depth/services/ctx";
+import { DepthService } from "../market-depth/services/depth.service";
+import { MarketDepth } from "../market-depth/view";
 
 const depthService = new DepthService();
 
